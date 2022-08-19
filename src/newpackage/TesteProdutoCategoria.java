ac66c93df0db3cd227874d8c9904a47c1db47aa4
Natália Leonel <leonel.natalia@aluno.ifsp.edu.br>
2022-08-19 15:32:08 -0300

    adicionando funcoes

--- src/newpackage/TesteProdutoCategoria.java
+++ src/newpackage/TesteProdutoCategoria.java
@@ -9,35 +9,52 @@ import java.util.List;
 
 public class TesteProdutoCategoria {
 
+    private static final ProdutoDAO produtoDAO = new ProdutoDAO();
+
     public static void main(String[] args) {
-        
-        
-        Categoria categoria = new Categoria();
-        categoria.setDescricao("Informatica");
-        
-        CategoriaDAO categoriaDAO = new CategoriaDAO();
-        categoriaDAO.inserir(categoria);
-        
-        Produto produto = new Produto();
-        produto.setDescricao("Teclado");
-        produto.setPreco(new BigDecimal(60.0));
-        produto.setQuantidade(100);
-        
-        produto.setCategoria(categoria);
-        
-        ProdutoDAO produtoDAO = new ProdutoDAO();
-        
-        
-        produtoDAO.inserir(produto);
-        
-      
-        List<Produto> produtos = produtoDAO.buscar();
-        for(Produto p: produtos){
-            System.out.println("Descricao: "+ p.getDescricao());
-            System.out.println("Categoria: "+ p.getCategoria().getDescricao());
+        adicionarProdutos();
+        exibirProdutos();
+        reajustarPreco(10.0);
+        exibirProdutos();
+
+    }
+
+    private static void adicionarProdutos() {
+        for (int i = 0; i < 10; i++) {
+            Categoria categoria = new Categoria();
+            categoria.setDescricao("Categoria " + i);
+            CategoriaDAO categoriaDAO = new CategoriaDAO();
+            categoriaDAO.inserir(categoria);
+
+            Produto produto = new Produto();
+            produto.setDescricao("Produto " + i);
+            produto.setPreco(new BigDecimal(60.0 * (i + 1)));
+            produto.setQuantidade(100);
+
+            produto.setCategoria(categoria);
+
+            produtoDAO.inserir(produto);
+
         }
-        
-       
-   }
+    }
+
+    private static void reajustarPreco(double percent) {
+        List<Produto> produtos = produtoDAO.buscar();
+        for (Produto p : produtos) {
+            p.setPreco(new BigDecimal(p.getPreco().doubleValue() + p.getPreco().doubleValue() * percent / 100));
+            produtoDAO.alterar(p);
+        }
+    }
+
+    private static void exibirProdutos() {
+        List<Produto> produtos = produtoDAO.buscar();
+        for (Produto p : produtos) {
+            System.out.println("Descri: " + p.getDescricao());
+            System.out.println("Preco: " + p.getPreco());
+            System.out.println("Categoria: " + p.getCategoria().getDescricao());
+            System.out.println("Estoque: " + p.getQuantidade());
+            System.out.println("");
+        }
+    }
 
 }
